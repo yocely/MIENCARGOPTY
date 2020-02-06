75c9c7a8f7e028d8a5f760a53dbab990bd79871c
yocely20 <yocelyascanio@outlook.ccom>
2020-02-05 23:26:03 -0500

    rrr

--- alime/popup.js
+++ alime/popup.js
@@ -13,3 +13,23 @@ btnCerrarPopup.addEventListener('click', function(e){
 	overlay.classList.remove('active');
 	popup.classList.remove('active');
 });
+
+var fecha = document.getElementById("test").value;
+var edad = calcularEdad(fecha);
+
+if(edad > 15){
+    // aqui haces lo que quieras con la validacion de si es mayor a 15
+    alert("El usuario es mayor a 15 años").
+}
+function calcularEdad(fecha) {
+    var hoy = new Date();
+    var cumpleanos = new Date(fecha);
+    var edad = hoy.getFullYear() - cumpleanos.getFullYear();
+    var m = hoy.getMonth() - cumpleanos.getMonth();
+
+    if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
+        edad--;
+    }
+
+    return edad;
+}
