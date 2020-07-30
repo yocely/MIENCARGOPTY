1f287e34112ae299ae733e5b89b648fd1eb1004a
yocely <44207888+yocely@users.noreply.github.com>
2020-07-30 17:28:54 -0500

    send email mejorado
    mejoras en el contactenos

--- alime/index.js
+++ alime/index.js
@@ -4,14 +4,13 @@ const exphbs = require('express-handlebars');
 const path = require('path');
 const nodemailer = require('nodemailer');
 const app = express();
+const { Router } = require('express');
+const router = Router();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(require('./routes/index'));
 // View engine setup
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-
 // Static folder
 app.use(express.static(path.join(__dirname)));
 
@@ -23,52 +22,49 @@ app.get('/', (req, res) => {
   res.render('contact');
 });
 
-app.post('/send-email', (req, res) => {
-  const output = `
-    <p>Ha recibido una nueva solicitud de contacto desde La Pagina WEB</p>
-    <h3>Datos del Cliente:</h3>
-    <ul>  
-      <li>Nombre: ${req.body.name}</li>
-      <li>Email: ${req.body.email}</li>
-      <li>Telefono: ${req.body.phone}</li>
-    </ul>
-    <h3>Mensaje</h3>
-    <p>${req.body.message}</p>
-  `;
-
+app.post('/send-email', async (req, res)=>{
+  const {name , email, phone, message}=req.body;  
+  contentHTML =`
+  <p>Ha recibido una nueva solicitud de contacto desde La Pagina WEB</p>
+  <h3>Datos del Cliente:</h3>
+  <ul>  
+    <li>Nombre: ${name}</li>
+    <li>Email: ${email}</li>
+    <li>Telefono: ${phone}</li>
+  </ul>
+  <h3>Mensaje</h3>
+  <p>${message}</p>
+`;
   // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: 'miencargopty.com',
+  const transporter = nodemailer.createTransport({
+
+    host: 'smtp.zoho.com',
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
-        user: 'miencargopty@gmail.com', // generated ethereal user
-        pass: '21012019'  // generated ethereal password
+        user: 'info@miencargopty.com', // generated ethereal user
+        pass: 'Panama$2020'  // generated ethereal password
     },
     tls:{
-      rejectUnauthorized:false
+        rejectUnauthorized: false
     }
+     
   });
+  const info = await transporter.sendMail({
 
-  // setup email data with unicode symbols
-  let mailOptions = {
-      from: '"Contactenos" <miencargopty.com>', // sender address
+    from: '"Contactenos" <info@miencargopty.com>', // sender address
       to: 'info@miencargopty.com', // list of receivers
       subject: 'SOLICITUD DE CONTACTO', // Subject line
-      text: 'Un potencial cliente quiere contactarte.', // plain text body
-      html: output // html body
-  };
-
-  // send mail with defined transport object
-  transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-          return console.log(error);
-      }
-      console.log('Message sent: %s', info.messageId);   
-      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-      res.render('contact', {msg:'Email has been sent'});
+      html: contentHTML
+     
   });
+
+  console.log('Mensaje Enviado', info.messageId);
+
+  console.log(contentHTML);
+    res.redirect('/mensaje.html');
+
   });
 
 app.listen(3000, () => console.log('Server started...'));
+module.exports = router;
