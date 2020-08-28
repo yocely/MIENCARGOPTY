ac3c5622b4e72877cdf043569c34d07f06275aa5
yocely <34fd41f8f44386966af3d0f6d9c9c368ee66fe71>
2020-08-28 15:58:58 -0500

    limpieza

--- alime/index.js
+++ alime/index.js
@@ -7,8 +7,6 @@ const nodemailer = require('nodemailer');
 const app = express();
 const { Router } = require('express');
 const flash = require('connect-flash');
-const mysql = require('mysql');
-const { database } = require('./keys');
 const session = require('express-session');
 const MysqlStore = require('express-mysql-session');
 const { extname } = require('path');
@@ -16,10 +14,8 @@ const router = Router();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(flash());
 app.use(bodyParser.json());
- 
-// Static folder
+ // Static folder
 app.use(express.static(path.join(__dirname)));
-
 //hbs
 app.set('alime', path.join(__dirname, 'alime'));
 app.engine('hbs', exphbs({
@@ -31,26 +27,12 @@ app.engine('hbs', exphbs({
 }))
 app.set('view engine', '.hbs');
 // middlewares
-app.use(session({
-  secret: 'Mi encargo pty',
-  resave: false,
-  saveUninitialized: false,
-  store: new MysqlStore(database)
-}));
 app.use(flash());
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-
-//Flash secuencias
-app.use((req, res, next) => {
-    app.locals.success = req.flash();
-    next();
-});
-
 app.use('/', require('./routes/index'));
 app.use(require('./routes/index'));
-
 app.get('/', (req, res) => {
   res.send('contact');
 });
@@ -70,8 +52,7 @@ app.post('/send-email', async (req, res)=>{
 `;
   // create reusable transporter object using the default SMTP transport
   const transporter = nodemailer.createTransport({
- 
-    host: 'smtp.zoho.com',
+     host: 'smtp.zoho.com',
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
@@ -81,22 +62,16 @@ app.post('/send-email', async (req, res)=>{
     tls:{
         rejectUnauthorized: false
     }
-     
-  });
+       });
   const info = await transporter.sendMail({
- 
-    from: '"Contactenos" <noreply@miencargopty.com>', // sender address
+     from: '"Contactenos" <noreply@miencargopty.com>', // sender address
       to: 'info@miencargopty.com', // list of receivers
       subject: 'SOLICITUD DE CONTACTO', // Subject line
       html: contentHTML
-      
-  });
-
+        });
   console.log('Mensaje Enviado', info.messageId);
     res.redirect('/mensaje.html');
-
   });
-
   // enviar correo de Contactenos
 app.post('/rec-email', async (req, res)=>{
   const {name , email, phone, message}=req.body;  
@@ -108,8 +83,7 @@ app.post('/rec-email', async (req, res)=>{
 `;
   // create reusable transporter object using the default SMTP transport
   const transporter = nodemailer.createTransport({
- 
-    host: 'smtp.zoho.com',
+     host: 'smtp.zoho.com',
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
@@ -119,21 +93,15 @@ app.post('/rec-email', async (req, res)=>{
     tls:{
         rejectUnauthorized: false
     }
-     
-  });
+       });
   const info = await transporter.sendMail({
- 
-    from: '"RECUPERACION" <noreply@miencargopty.com>', // sender address
+     from: '"RECUPERACION" <noreply@miencargopty.com>', // sender address
       to: 'info@miencargopty.com', // list of receivers
       subject: 'SOLICITUD DE RECUPERAR CASILLERO', // Subject line
       html: contentHTML
-      
-  });
-
+        });
   console.log('Mensaje Enviado', info.messageId);
     res.redirect('/not.html');
-
   });
-
 app.listen(3000, () => console.log('Servidor iniciado...'));
 module.exports = router;
--- alime/routes/index.js
+++ alime/routes/index.js
@@ -8,20 +8,18 @@ router.post('/sent-email', async (req, res) => {
     contentHTML = `
     <p>Te Saluda tu amigo MI ENCARGO PTY, en este correo te dictamos tu casillero asignado el cual es:</h3>
     <ul>  
-      <h2>Nombre: ${nombre} ${apellido} / MIENCARGO</h2>
-      <h2>Direccion: 1345 NW 98TH CT, ST2 Miami, Florida </h2>
-      <h2>Codigo Postal: 33172-2779 </h2>
-      <h2>Teléfono: 786 360-2816</h2>
+      <h3>Nombre: ${nombre} ${apellido} / MIENCARGO</h3>
+      <h3>Direccion: 1345 NW 98TH CT, ST2 Miami, Florida </h3>
+      <h3>Codigo Postal: 33172-2779 </h3>
+      <h3>Teléfono: 786 360-2816</h3>
     </ul>
     <p>Por favor copia y pega estos datos en tu pagina de compra y disfruta de tu casillero desde YA!!</p>
     <p>Recuerda siempre enviarnos el numero de tracking o rastreo de tus paquetes para una mayor atencion.</p>
-    <p>Cualquiera duda o consuta puedes contactarnos al Whatsapp: +507 6289-7140 o al correo: info@miencargopty.com</p>
-    <br>
+    <h4>Cualquiera duda o consuta puedes contactarnos al Whatsapp: +507 6289-7140 o al correo: info@miencargopty.com</h4>
     <p>MI ENCARGO PTY no asume responsabilidad por ningún paquete o artículo transportado o entregado a nosotros por USPS, dado que no hay constancia de estatus en el tiempo real de las entregas, ni prueba de firma por parte de dicha compañía por ende no aceptamos ningún tipo de reclamos ni cargo de responsabilidad y queda a criterio del cliente si usa ese servicio en los estados unidos.</p>
    <br>
-    <h3>Los datos ingresados fueron:</h3>
-    <p>Esta informacion que mostramos abajo es para que te pongas en contacto con nosotros si has ingresado algun campo mal</p>
-  <ul>  
+    ` 
+    contentHTML2 = `<ul>  
     <li>Nombre: ${nombre}</li> <br>
     <li>Apellido: ${apellido}</li> <br>
     <li>Cedula: ${cedula}</li> <br>
@@ -30,8 +28,7 @@ router.post('/sent-email', async (req, res) => {
     <li>Telefono: ${celular}</li> <br>
     <li>Direccion: ${direccion}</li> <br>
     <li>Pais: ${pais}</li> <br>
-  </ul> 
-    `;
+  </ul> `;
     const transporter = nodemailer.createTransport({
         host: 'smtp.zoho.com',
         port: 587,
@@ -48,94 +45,19 @@ router.post('/sent-email', async (req, res) => {
        
         from: '"No Reply" <noreply@miencargopty.com>', // sender address
         to: email,// list of receivers
-        cc: 'info@miencargopty.com',
         subject: 'CASILLERO MI ENCARGO PTY', // Subject line
         html: contentHTML
     })
-    //req.flash('success', 'Usuario Registrado satisfactoriamente, Por favor revisa tu correo eletrónico.');
+
+    const info2 = await transporter.sendMail({
+       
+        from: '"NUEVO CLINTE" <noreply@miencargopty.com>', // sender address
+        to: 'info@miencargopty.com',// list of receivers
+        subject:'NUEVO CLINTE REGISTRADO ', // Subject line
+        html: contentHTML2
+    })
+
     res.redirect('/mens.html');
 });
 module.exports = router;
- //BASE DE DATOS
-const pool = require('../databse');
-router.get('/', (req, res) => {
-    res.render('index.html');
-});
-router.get('/registro.html', (req, res) => {
-    res.render('registro.html');
-});
-router.get('/tarifas.html', (req, res) => {
-    res.render('tarifas.html');
-});
-router.get('/casillero.html', (req, res) => {
-    res.render('casillero.html');
-});
-router.get('/rastreo.html', (req, res) => {
-    res.render('rastreo.html');
-});
-router.get('/calculadora.html', (req, res) => {
-    res.render('calculadora.html');
-});
-router.get('/pagos.html', (req, res) => {
-    res.render('pagos.html');
-});
-router.get('/terminos.html', (req, res) => {
-    res.render('terminos.html');
-});
-router.get('/nosotros.html', (req, res) => {
-    res.render('nosotros.html');
-});
-router.get('/contactos.html', (req, res) => {
-    res.render('contactos.html');
-});
-router.post('/registro.html', async (req, res) => {
-        const { id, nombre, apellido, cedula, fecha_de_nacimiento, email, celular, direccion, pais } = req.body;
-        const newLink = {
-            id,
-            nombre,
-            apellido,
-            cedula,
-            fecha_de_nacimiento,
-            email,
-            celular,
-            direccion,
-            pais
-        }
-        await pool.query('INSERT INTO users set ?', [newLink]);
-        console.log("'Registaro!!"); 
-        res.redirect('/mens.html');
-    });
-router.get('/', async (req, res) => {
-    const links = await pool.query('SELECT * FROM users');
-    console.log(links);
-    res.render('/list.html', { links });
-});
-router.get('/delete/:id', async (req, res) => {
-    const { id } = req.params;
-    await pool.query('DELETE FROM users WHERE ID = ?', [id]);
-    //req.flash('success', 'Usuario Eliminado Removido satisfactoriamente');
-    res.redirect('/index.html');
-});
-router.get('/edit/:id', async (req, res) => {
-    const { id } = req.params;
-    const links = await pool.query('SELECT * FROM users WHERE id = ?', [id]);
-    res.render('/edit.html', { link: links[0] });
-});
-router.post('/edit/:id', async (req, res) => {
-    const { id } = req.params;
-    const { nombre, apellido, cedula, fecha_de_nacimiento, email, celular, direccion, pais } = req.body;
-    const newLink = {
-        nombre,
-        apellido,
-        cedula,
-        fecha_de_nacimiento,
-        email,
-        celular,
-        direccion,
-        pais
-    }
-    await pool.query('UPDATE users set ? WHERE id = ?', [newLink, id]);
-    //req.flash('success', 'Usuario actualizado satisfactoriamente');
-    res.redirect('/mens.html');
-});
-module.exports = router;
+ 
